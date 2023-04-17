ce2538bc1d09b98d7fe36c06882812322ba290bf
a <yhma22.alkeal@folkuniversitetet.nu>
2023-04-17 15:55:59 +0200

    Initial Commit

---
+++ newHabits/newHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  newHabitsApp.swift
+//  newHabits
+//
+//  Created by A on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct newHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
